b5d011654ecd72e909b632ba15807865c4063891
Travis Schriner <C15Travis.Schriner@usafa.edu>
2014-01-31 14:23:40 -0700

    syntactically correct v_sync_gen

--- Lab1_VGAController/v_sync_gen.vhd
+++ Lab1_VGAController/v_sync_gen.vhd
@@ -40,8 +40,111 @@ end v_sync_gen;
 
 architecture Behavioral of v_sync_gen is
 
+
+	--had to make my own type...
+	type states is (activeVid, frontPorch, sync, backPorch);
+	signal state_reg, state_next : states;
+	signal count_reg, count_next : unsigned (10 downto 0);
+	signal donezoes : std_logic;
 begin
 
+
+   --state reg
+	process(clk, reset)
+	begin
+		if(reset ='1') then
+			state_reg <= activeVid;
+		elsif( rising_edge(clk)) then
+			state_reg <= state_next;
+		end if;
+	end process;
+	
+	
+	
+	--count reg
+	process(h_completed, reset)
+	begin
+		if (reset = '1') then
+			count_reg <= (others => '0');
+		elsif( rising_edge(h_completed)) then
+			if(state_reg = state_next) then
+				count_reg <= count_next;
+			else 
+				count_reg <= (others => '0');
+			end if;
+		end if;
+	end process;
+	
+	
+	
+	--next count logic
+	process( state_reg, state_next, count_reg)
+	begin 
+	
+		if (state_next = state_reg) then 
+			count_next <= count_reg +1;
+		else 
+			count_next <= (others => '0');
+		end if;
+	end process;
+	
+	
+	
+	process(state_reg)
+	begin
+		case state_reg is
+		
+		when backPorch =>
+			if (count_reg < 33) then
+				state_next <= backPorch;
+				donezoes <= '0';
+			else 
+				state_next <= activeVid;
+				donezoes <= '1';
+			end if;
+		
+		when sync =>
+			if (count_reg < 2) then
+				state_next <= sync;
+				donezoes <= '0';
+			else 
+				state_next <= backPorch;
+				donezoes <= '0';
+			end if;
+		
+		when frontPorch =>
+			if(count_reg < 10) then
+				state_next <= frontPorch;
+				donezoes <= '0';
+			else 
+				state_next <= sync;
+				donezoes <= '0';
+			end if;
+
+		
+		when activeVid =>
+			if(count_reg <480) then
+				state_next <= activeVid;
+				donezoes <= '0';
+			else 
+				state_next <= frontPorch;
+				donezoes <= '0';
+			end if;
+			
+	   end case;
+		
+	end process;
+	
+	--output logic
+		v_sync <= '0' when state_reg = sync else
+					 '1';
+		blank <= '0' when state_reg = activeVid else
+					'1';
+		completed <= donezoes;
+		
+		row <=  count_reg when state_reg = activeVid else
+					(others => '0');
+	
 
 end Behavioral;
 
